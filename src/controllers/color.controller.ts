23c54dc9f70c8f584e93250bed59ed894c6b62f2
Juan Sanchez <ja.sanchezromero.dev@gmail.com>
2026-02-25 19:21:51 -0500

    controlador de color

---
+++ src/controllers/color.controller.ts
@@ -0,0 +1,81 @@
+import { Request, Response } from "express";
+import sql from "../db/supabase";
+
+export async function getColor(req: Request, res: Response) {
+    try {
+        const colors = await sql`SELECT * FROM COLOR ORDER BY name`;
+        res.json(colors);
+    } catch (error) {
+        res.status(500).json({ error: "Error al obtener los colores" });
+    }
+}
+
+export async function postColor(req: Request, res: Response) {
+    try {
+        const { name, codigo, slug, sku } = req.body;
+        const newColor = await sql`INSERT INTO COLOR (name, codigo, slug, sku) VALUES (${name}, ${codigo}, ${slug}, ${sku})`;
+        res.status(201).json(newColor[0]);
+    } catch (error) {
+        res.status(500).json({ error: "Error al crear el color" });
+    }
+}
+
+export async function deleteColor(req: Request, res: Response) {
+    try {
+        const { id } = req.params;
+
+        await sql`
+            UPDATE COLOR SET is_deleted = true WHERE id = ${id}`;
+
+        res.status(204).send();
+    } catch (error) {
+        res.status(500).json({ error: "Error al eliminar el color" });
+    }
+}
+
+export async function putColor(req: Request, res: Response) {
+    try {
+        const { id } = req.params;
+        const { name, codigo, slug, sku } = req.body;
+        const updatedColor = await sql`UPDATE COLOR SET name = ${name}, codigo = ${codigo}, slug = ${slug}, sku = ${sku} WHERE id = ${id} RETURNING *`;
+        res.json(updatedColor[0]);
+    } catch (error) {
+        res.status(500).json({ error: "Error al actualizar el color" });
+    }
+}
+
+export async function patchColor(req: Request, res: Response) {
+    try {
+        const { id } = req.params;
+        const { name, codigo, slug, sku, is_deleted } = req.body;
+
+        const fields = [];
+
+        if (name !== undefined) {
+            fields.push(sql`name = ${name}`);
+        }
+        if (codigo !== undefined) {
+            fields.push(sql`codigo = ${codigo}`);
+        }
+        if (slug !== undefined) {
+            fields.push(sql`slug = ${slug}`);
+        }
+        if (sku !== undefined) {
+            fields.push(sql`sku = ${sku}`);
+        }
+        if (is_deleted !== undefined) {
+            fields.push(sql`is_deleted = ${is_deleted}`);
+        }
+
+        if (fields.length === 0) {
+            return res.status(400).json({ error: "No hay campos para actualizar" });
+        }
+
+        const updatedColor = await sql` UPDATE COLOR SET ${sql.join(fields, sql`, `)} WHERE id = ${id} RETURNING *`;
+
+        res.json(updatedColor[0]);
+
+    } catch (error) {
+        res.status(500).json({ error: "Error al actualizar parcialmente el color" });
+    }
+}
--- src/routes/color.route.ts
+++ src/routes/color.route.ts
@@ -1,11 +1,12 @@
-import { Router} from "express"
+import { Router } from "express"
+import { deleteColor, getColor, patchColor, postColor, putColor } from "../controllers/color.controller"
 
 const router = Router()
 
 router.get("/", getColor)
-router.post("/",postColor)
+router.post("/", postColor)
 router.put("/:id", putColor)
-router.delete("/:id",deleteColor)
+router.delete("/:id", deleteColor)
 router.patch("/:id", patchColor)
 
 export default router
