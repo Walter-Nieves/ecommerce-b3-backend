19eac260e5d0f7f15d977e09c49745a53ca8ce9c
walter <nievesarrietawalter@gmail.com>
2026-03-18 12:13:21 -0500

    Creacion de product.controller y product.route

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -22,40 +22,40 @@ import { BucketRoutes } from "../types/enums";
 
 const router = Router();
 
-// 📌 GET
-router.get("/all", authVerify(true), getAllUsers); //✅
-router.get("/all-deleted", authVerify(true), getDeletedUsers); //✅
-router.get("/id/deleted/:id", authVerify(true), getDeletedUserById); //✅
-router.get("/id/:id", authVerify(true), getUserById); //✅
-router.get("/email/deleted/:email", authVerify(true), getDeletedUserByEmail); //✅
-router.get("/email/:email", authVerify(true), getUserByEmail); //✅
-router.get("/me", authVerify(true), getUserMe); //✅
+// GET
+router.get("/all", authVerify(true), getAllUsers);
+router.get("/all-deleted", authVerify(true), getDeletedUsers);
+router.get("/id/deleted/:id", authVerify(true), getDeletedUserById);
+router.get("/id/:id", authVerify(true), getUserById);
+router.get("/email/deleted/:email", authVerify(true), getDeletedUserByEmail);
+router.get("/email/:email", authVerify(true), getUserByEmail);
+router.get("/me", authVerify(true), getUserMe);
 
-// 📌 CREATE
+// CREATE
 router.post(
   "/",
   authVerify(false),
   uploadImage(BucketRoutes.UserImages),
   createUser,
-); //✅
+);
 
 router.post("/check-email", postCheckEmail);
 
-// 📌 UPDATE
-router.put("/:id", authVerify(true), updateUser); //✅
+// UPDATE
+router.put("/:id", authVerify(true), updateUser);
 
-// 📌 DELETE
-router.delete("/soft/:id", authVerify(true), softDeleteUser); //✅
-router.delete("/force/:id", authVerify(true), forceDeleteUser); //✅
+// DELETE
+router.delete("/soft/:id", authVerify(true), softDeleteUser);
+router.delete("/force/:id", authVerify(true), forceDeleteUser);
 
-// 📌 PATCH
-router.patch("/restore/:id", authVerify(true), restoreUser); //✅
+// PATCH
+router.patch("/restore/:id", authVerify(true), restoreUser);
 router.patch(
   "/photo/:id",
   authVerify(true),
   uploadImage(BucketRoutes.UserImages),
   updatePhotoUser,
-); //✅
+);
 router.patch("/password/:id", authVerify(true), updatePasswordUser);
 
 export default router;
